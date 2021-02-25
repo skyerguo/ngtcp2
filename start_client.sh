6723bf6e3b1e971c6bd98491479696e353377c19
skyerguo <skyerguo97@gmail.com>
2021-02-25 20:59:37 +0800

    update

--- start_client.sh
+++ start_client.sh
@@ -1,2 +1,4 @@
 ./examples/client 127.0.0.1 4433 -i -w img -q
-/home/gtc/data/client 10.142.15.230 4433 -i -w img -q
+# /home/gtc/data/client 10.142.0.4 4433 -i -w img -q
+# /home/gtc/data/client 35.227.46.23 4433 -i -w instagram.com -q
+# /tmp/hestia/data/client 35.227.46.23 4433 -i -w instagram.com -q
