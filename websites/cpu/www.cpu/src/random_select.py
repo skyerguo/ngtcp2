7b9572aa4e95e161be9fe15a5bf97be8e5415592
skyerguo <skeyerguo97@gmail.com>
2021-03-19 03:46:01 +0000

    update

---
+++ websites/cpu/www.cpu/src/random_select.py
@@ -0,0 +1,13 @@
+import pymongo
+import random
+
+client = pymongo.MongoClient('localhost', 27017)
+db = client['motivation_index']
+collection = db['shuffle']
+
+n = random.randint(10, 60)
+print(n)
+
+for i in range(n):
+    for item in collection.find({"value": random.randint(0, 5000000)}):
+        print(item)
