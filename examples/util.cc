c39f4c8fe99165dfb0bc501b099b4a057bb761a6
skyerguo97 <skyerguo97@gmail.com>
2022-01-08 22:22:06 +0800

    修改server获取client的id的逻辑

--- examples/util.cc
+++ examples/util.cc
@@ -243,8 +243,7 @@ std::string int2Address(uint64_t ip_int) {
 
 std::string getUinque(const uint64_t &a, const uint64_t &b, const uint64_t &c) {
   std::string res = "";
-  // res = int2Address(a) + "_";
-  res = std::to_string((a%10000)/1000) + "_"; // 假设client数量少于10个，用倒数第二位表示id
+  res = std::to_string((a%1000000)/100000 + (a%100000)/10000 + (a%10000)/1000) + "_"; // 假设client数量少于1000个，用三位数表示表示id
   res += std::to_string(b) + "_";
   res += std::to_string(c);
   return res;
